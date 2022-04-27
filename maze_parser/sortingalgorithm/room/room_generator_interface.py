a78f7fc5fea1af5ba8d46777060a6e889715e979
millipedes <millipedes@earthlink.net>
2022-04-26 21:29:37 -0600

    update commit

--- maze_parser/sortingalgorithm/room/room_generator_interface.py
+++ maze_parser/sortingalgorithm/room/room_generator_interface.py
@@ -1,3 +1,15 @@
 from . import room
+from . import room_data
 
-def 
+def make_room_a():
+    room_data = room_data()
+    room_data.turn_off_right()
+    room_data.turn_off_left()
+    room = Room(room_data=room_data)
+    return room
+
+def make_room_b(): 
+def make_room_c(): 
+def make_room_d(): 
+def make_room_f(): 
+def make_room_g(): 
--- maze_parser/sortingalgorithm/wilsons_algorithm.py
+++ maze_parser/sortingalgorithm/wilsons_algorithm.py
@@ -23,19 +23,6 @@ class wilsons_algorithm(sorting_algorithm):
         y_start = rand.randrange(0, self.h) # Set Notation-> [0, (h - 1)]
         x_curr  = random_x_start            # start x at the x start
         y_curr  = random_y_start            # start y at the y start
-        while not ((x_end != x_curr) and (y_end != y_curr)):
-            choice = rand.randrange(1, 5) # make a choice of direction
-            if choice == 1 and (y_curr - 1 >= 0):
-                y_curr -= 1
-            # If we chose to go right and we can still go right
-            elif choice == 2 and (x_curr + 1 < self.w):
-                x_curr += 1
-            # If we chose to go down and we can still go down
-            elif choice == 3 and (y_curr + 1 < self.h):
-                y_curr += 1
-            # If we chose to go left and we can still go left
-            elif choice == 4 and (x_curr - 1 >= 0):
-                x_curr -= 1
-            if(head_room.room_data == None)
+        head_room = wilsons_algorithm(head_room, x_start, x_end, y_start, y_end)
 
-    def wilsons_algorithm(self, x_curr, x_end, y_curr, y_end):
+    def wilsons_algorithm(self, room, x_start, x_end, y_start, y_end):
