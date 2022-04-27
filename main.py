de2e1758e7202a112e578df6e52adcdaab44b2a6
millipedes <millipedes@earthlink.net>
2022-04-26 22:08:42 -0600

    update commit

--- main.py
+++ main.py
@@ -128,48 +128,6 @@ def my_mat_trans(M, x_start, y_start, x_end, y_end, w, h):
         M_final[y_end][x_end] = "h"
     return M_final
 
-def wilsons_algorithm(h, w):
-    M = [[0 for _ in range(w)] for _ in range(h)]
-    rand.seed()
-    random_x_dest  = rand.randrange(0, w) # Set Notation-> [0, (w - 1)]
-    random_y_dest  = rand.randrange(0, h) # Set Notation-> [0, (h - 1)]
-    random_x_start = rand.randrange(0, w) # Set Notation-> [0, (w - 1)]
-    random_y_start = rand.randrange(0, h) # Set Notation-> [0, (h - 1)]
-    current_x_coor = random_x_start            # start x at the x start
-    current_y_coor = random_y_start            # start y at the y start
-    rand.seed()                                # start random engine
-    while not (current_x_coor == random_x_dest and current_y_coor ==
-            random_y_dest):
-        choice = rand.randrange(1, 5) # make a choice of direction
-        M[current_y_coor][current_x_coor] = choice # Choice of direction
-        if choice == 1 and (current_y_coor - 1 >= 0):
-            current_y_coor -= 1
-        # If we chose to go right and we can still go right
-        elif choice == 2 and (current_x_coor + 1 < w):
-            current_x_coor += 1
-        # If we chose to go down and we can still go down
-        elif choice == 3 and (current_y_coor + 1 < h):
-            current_y_coor += 1
-        # If we chose to go left and we can still go left
-        elif choice == 4 and (current_x_coor - 1 >= 0):
-            current_x_coor -= 1
-    if choice == 1:
-        M[current_y_coor][current_x_coor] = 5
-    if choice == 2:
-        M[current_y_coor][current_x_coor] = 6
-    if choice == 3:
-        M[current_y_coor][current_x_coor] = 7
-    if choice == 4:
-        M[current_y_coor][current_x_coor] = 8
-    P = [] # Path
-    path_Mat = [[0 for _ in range(w)] for _ in range(h)]
-    get_path(P, M, random_x_start, random_y_start, random_x_dest,
-            random_y_dest)
-    print(P)
-    path_to_mat(P, path_Mat, w, h)
-    M_final = my_mat_trans(path_Mat, random_x_start, random_y_start,
-            random_x_dest, random_y_dest, w, h)
-    return M_final
 
 q = 5
 M = wilsons_algorithm(q, q)
--- maze_parser/sortingalgorithm/wilsons_algorithm.py
+++ maze_parser/sortingalgorithm/wilsons_algorithm.py
@@ -21,8 +21,61 @@ class wilsons_algorithm(sorting_algorithm):
         y_end   = rand.randrange(0, self.h) # Set Notation-> [0, (h - 1)]
         x_start = rand.randrange(0, self.w) # Set Notation-> [0, (w - 1)]
         y_start = rand.randrange(0, self.h) # Set Notation-> [0, (h - 1)]
-        x_curr  = random_x_start            # start x at the x start
-        y_curr  = random_y_start            # start y at the y start
-        head_room = wilsons_algorithm(head_room, x_start, x_end, y_start, y_end)
+        x_curr  = x_start                   # start x at the x start
+        y_curr  = y_start                   # start y at the y start
+        head_room = wilsons_algorithm(head_room, x_start, x_end, x_start,
+                y_start, y_end, y_start)
 
-    def wilsons_algorithm(self, room, x_start, x_end, y_start, y_end):
+    def wilsons_algorithm(self, room, x_start, x_end, x, y_start, y_end, y):
+        M = self.preleminary_matrix_gen()
+
+
+    def preleminary_matrix_gen(self):
+        M = [[0 for _ in range(self.w)] for _ in range(self.h)]
+        rand.seed()
+        x_end  = rand.randrange(0, self.w)  # Set Notation-> [0, (w - 1)]
+        y_end  = rand.randrange(0, self.h)  # Set Notation-> [0, (h - 1)]
+        x_start = rand.randrange(0, self.w) # Set Notation-> [0, (w - 1)]
+        y_start = rand.randrange(0, self.h) # Set Notation-> [0, (h - 1)]
+        x_curr = x_start                    # start x at the x start
+        y_curr = y_start                    # start y at the y start
+        rand.seed()                         # start random engine
+        while not (x_curr == x_end and y_curr == y_end):
+            choice = rand.randrange(1, 5) # make a choice of direction
+            M[y_curr][x_curr] = choice # Choice of direction
+            if choice == 1 and (y_curr - 1 >= 0):
+                y_curr -= 1
+            # If we chose to go right and we can still go right
+            elif choice == 2 and (x_curr + 1 < w):
+                x_curr += 1
+            # If we chose to go down and we can still go down
+            elif choice == 3 and (y_curr + 1 < h):
+                y_curr += 1
+            # If we chose to go left and we can still go left
+            elif choice == 4 and (x_curr - 1 >= 0):
+                x_curr -= 1
+        if choice == 1:
+            M[y_curr][x_curr] = 5
+        if choice == 2:
+            M[y_curr][x_curr] = 6
+        if choice == 3:
+            M[y_curr][x_curr] = 7
+        if choice == 4:
+            M[y_curr][x_curr] = 8
+        P = [] # Path
+        get_path(P, M, x_start, y_start, x_end, y_end)
+        path_to_mat(P, path_Mat, w, h)
+        return M
+    def path_to_mat(P, M, w, h):
+        for i in range(len(P)):
+            M[P[i][1]][P[i][0]] = P[i][2]
+    def get_path(A, M, x, y, x_end, y_end):
+        A.append((x, y, M[y][x]))
+        if M[y][x] == 1:
+            get_path(A, M, x, y - 1, x_end, y_end)
+        if M[y][x] == 2:
+            get_path(A, M, x + 1, y, x_end, y_end)
+        if M[y][x] == 3:
+            get_path(A, M, x, y + 1, x_end, y_end)
+        if M[y][x] == 4:
+            get_path(A, M, x - 1, y, x_end, y_end)
